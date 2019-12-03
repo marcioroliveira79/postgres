c08c14ddb318371cd89cff8e5bdae57a87afa320
marcioroliveira79 <marcio.r.oliveira79@gmail.com>
2019-12-03 15:20:14 -0300

    Add files via upload

---
+++ DesbilitarIndiceTabela.sql
@@ -0,0 +1,9 @@
+--Disabilitar indices de tabelas com update
+-- 03-12-2019
+UPDATE pg_index
+SET indisready=false
+WHERE indrelid = (
+    SELECT oid
+    FROM pg_class
+    WHERE relname='<TABLE_NAME>'
+);
